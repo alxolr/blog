d58e6c7d46d96c9c426cac3c60f210204bd420e0
Alexandru Olaru <alxolr@gmail.com>
2018-10-06 16:11:37 +0300

    Added the APP_CONFIG variable

--- config/production.js
+++ config/production.js
@@ -1,4 +1,8 @@
 'use strict';
 
 module.exports = {
+  port: process.env.PORT || 8080,
+  mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017/articles',
+  limit: 6
 };
+
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,11 +8,13 @@ module.exports = {
       watch: true,
       env: {
         PORT: 8080,
-        NODE_ENV: 'development'
+        NODE_ENV: 'development',
+        NODE_APP_INSTANCE: 'development'
       },
       env_production: {
         PORT: 8080,
         NODE_ENV: 'production',
+        NODE_APP_INSTANCE: 'production'
       }
     }
   ]
