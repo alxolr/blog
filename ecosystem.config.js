7bb0b545e8abc933f22bfd58d402c2546df6cb8a
Alexandru Olaru <alxolr@gmail.com>
2018-10-06 15:58:41 +0300

    Added the ecosystem file for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+  apps: [
+    {
+      name: 'blog',
+      script: './index.js',
+      watch: true,
+      env: {
+        PORT: 8080,
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        PORT: 80,
+        NODE_ENV: 'production',
+      }
+    }
+  ]
+};
